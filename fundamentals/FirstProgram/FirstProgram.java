b491ce096f61d3d3fbdad884d019de83f8ba1de5
schuang25 <stephen.chuang25@gmail.com>
2022-02-07 13:17:33 -0800

    first program, cafe java

---
+++ fundamentals/FirstProgram/FirstProgram.java
@@ -0,0 +1,7 @@
+public class FirstProgram {
+    public static void main (String [] args) {
+        System.out.println("My name is Stephen Chuang");
+        System.out.println("I am 25 years old");
+        System.out.println("My hometown is Cupertino, CA");
+    }
+}
