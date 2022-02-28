659311b034587a056898a1ab3f0556b6fbf35056
schuang25 <stephen.chuang25@gmail.com>
2022-02-28 10:21:34 -0800

    w3d5 algo update

---
+++ algos/w3d5.js
@@ -0,0 +1,109 @@
+// DLLNodes have a .next and .prev
+class DLLNode {
+    constructor(data) {
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+    }
+}
+// ⚠ other methods removed for brevity ⚠
+// DLLists have both a .head and .tail pointer
+class DLList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    // == Main Methods ==
+
+    // return true or false if the current linked list is a palindrome
+    // a palindrome is a string of characters equal to itself when reversed
+    // assume your node.data are all numbers or lowercase chars
+    isPalindrome() { 
+        if (this.isEmpty())
+            return false;
+        if (this.head === this.tail)
+            return true;
+        
+    }
+    // reverse a doubly linked list in place
+    reverse() { }
+    // ---------------------------
+    // remove and return the first node with data === val, if it exists
+    // what if the list is empty?
+    // what if the target val is the head?
+    // what if the target val is the tail?
+    // what if the target val is the only node in the list?
+    removeVal(val) { }
+
+    prependClean(target, node) {
+        var runner = this.head; // set a runner
+        if (runner.data === target) {
+            this.addHead(node);
+            return;
+        }
+        while (runner) { // loop
+            if (runner.data !== target) { // check runner data against the target
+                runner = runner.next;     // move forward if no match
+            } else {                      // else we found a match
+                node.next = runner;       // point the node at the matched runner
+                node.prev = runner.prev;  // point the node's prev to the matched runner's prev
+                node.prev.next = node;    // link previous node next
+                runner.prev = node;       // link runner to node
+                return;
+            }
+        }
+    }
+
+    // return true or false if t a node exists with data === val
+    exists(val) {
+        var forwardRunner = this.head;
+        var backwardRunner = this.tail;
+        var count = Math.ceil(this.length / 2);
+        while (count) {
+            if (forwardRunner.data === val || backwardRunner.data === val) {
+                return true;
+            }
+            forwardRunner = forwardRunner.next;
+            backwardRunner = backwardRunner.prev;
+            count--;
+        }
+        return false;
+    }
+    // push to head
+    addHead(node) {
+        if (!this.head) { // empty list
+            this.head = node;
+            this.tail = node;
+        } else {
+            this.head.prev = node;
+            node.next = this.head;
+            this.head = node;
+
+            // this.tail.next = node;
+            // node.prev = this.tail;
+            // this.tail = node;
+        }
+    }
+
+    // pop from tail
+    removeTail() {
+        if (this.head == null) return; // empty list
+        if (this.head === this.tail) { // one node
+            var temp = this.tail; // set a temp
+            this.head = null; // disconnect the head
+            this.tail = null; // disconnect the tail
+            return temp;
+        }
+        var temp = this.tail; // set a temp
+        this.tail = tail.prev; // move the tail back
+        this.tail.next = null; // null out the new tail's next
+        temp.prev = null; // null out the temp's prev
+        return temp;
+    }
+
+    // return is empty
+    isEmpty() {
+        return this.head === null;
+    }
+}
